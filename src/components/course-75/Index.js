1df1d92181bf20fc41111fd2b3f9f41b2bc7417b
thelast7 <takimanwar@gmail.com>
2019-02-07 15:01:25 +0700

    latihan async await

--- src/components/course-75/Index.js
+++ src/components/course-75/Index.js
@@ -1,12 +1,29 @@
 import React from "react";
+import axios from "axios";
 import Search from "./Search";
 
-const Index = () => {
-  return (
-    <div>
-      <Search />
-    </div>
-  );
-};
+class Index extends React.Component {
+  async onSubmitIt(text) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: text },
+      headers: {
+        Authorization:
+          "Client-ID c4745c8f63217540d9877547ec1272f3afd43b0bf49e21f00018b6ec653e55cd"
+      }
+    });
+    console.log(response.data.results);
+    // .then(response => {
+    //   console.log(response.data.results);
+    // });
+  }
+
+  render() {
+    return (
+      <div>
+        <Search onSubmit={this.onSubmitIt} />
+      </div>
+    );
+  }
+}
 
 export default Index;
--- src/components/course-75/Search.js
+++ src/components/course-75/Search.js
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
 
 class Search extends Component {
-  onInputChange(e) {
-    console.log(e.target.value);
-  }
+  // onInputChange(e) {
+  //   console.log(e.target.value);
+  // }
+  state = { text: "" };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.text);
+  };
 
   render() {
     return (
       <div className="ui segment container">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label htmlFor="">Cari Gambar</label>
-            <input onChange={this.onInputChange} />
+            {/* <input onChange={this.onInputChange} /> */}
+            {/* <input onChange={e => console.log(e.target.value)} /> //sama aja kaya diatas             */}
+            <input
+              type="text"
+              value={this.state.text}
+              onChange={e => this.setState({ text: e.target.value })}
+            />
           </div>
         </form>
       </div>
