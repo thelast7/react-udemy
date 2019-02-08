647220ae43bc017de32a26a0566c56b68dbfdd9d
thelast7 <takimanwar@gmail.com>
2019-02-08 19:33:46 +0700

    nambah card

--- src/components/course-75/Index.js
+++ src/components/course-75/Index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Api from "./api/Api";
 import Search from "./Search";
+import ImageList from "./ImageList";
 
 class Index extends React.Component {
   state = { images: [] };
 
   onSubmitIt = async text => {
-    const response = await Api.get("search/photos", {
+    const response = await Api.get("/search/photos", {
       params: { query: text }
     });
     // .then(response => {
@@ -21,6 +22,7 @@ class Index extends React.Component {
       <div className="ui container">
         <Search onSubmit={this.onSubmitIt} />
         total : {this.state.images.length}
+        <ImageList images={this.state.images} />
       </div>
     );
   }
